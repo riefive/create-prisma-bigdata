530006e3008e8470181747a5b21468dce4a53e8b
riefive <muhammad.rifai@salt.co.id>
2024-01-04 18:02:26 +0700

    feat(db): add function to find post

---
+++ find-query.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+    console.time('find');
+    const posts = await prisma.post.findMany({
+        select: {
+            id: true,
+            slug: true,
+            title: true,
+            content: true,
+            createdAt: true,
+            createdBy: true
+        },
+        skip: 0,
+        take: 50000,
+    });
+    if (posts.length > 0) console.log(posts[0]);
+    console.log(posts.length);
+    console.timeEnd('find');
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (error) => {
+        console.error(error);
+        await prisma.$disconnect();
+        process.exit(1);
+    })
