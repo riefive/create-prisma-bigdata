ec4e38d3dd93c108e88724e138312d8e311cc6f9
riefive <muhammad.rifai@salt.co.id>
2024-01-05 14:18:44 +0700

    feat(db): modify find query function

--- command-query.ts
+++ command-query.ts
@@ -3,16 +3,32 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+const isShowPost = false;
 const length = argv.length || 0;
 const menuParams = length === 3 ? argv[length - 1] : '--menu=normal';
+let menu = 'normal';
+
+switch (menuParams) {
+    case '--menu=normal':
+        menu = 'normal';
+        break;
+    case '--menu=cursor':
+        menu = 'cursor';
+        break;
+    case '--menu=byid':
+        menu = 'byid';
+        break;
+}
 
 async function main() {
-    console.time('command-find');
+    const keyword = `command-query-${menu}`;
+    console.time(keyword);
     const limit = 10000;
     const postDataTotal = await prisma.posts.count();
     const postPageTotal = Math.ceil(postDataTotal / limit);
     let i = 0;
     while (i < postPageTotal) {
+        let lastId = 1;
         const options: any = {
             select: {
                 id: true,
@@ -22,21 +38,40 @@ async function main() {
                 createdAt: true,
                 createdBy: true
             },
+            orderBy: {
+                id: 'asc',
+            },
             skip: i * limit,
             take: limit,
         };
+        if (menu === 'cursor') {
+            Object.assign(options, {
+                cursor: {
+                    id: lastId,
+                },
+            });
+        } else if (menu === 'optimize') {
+            Object.assign(options, {
+                where: { id: { gte: lastId }},
+            });
+        }
         const posts = await prisma.posts.findMany(options);
         console.log(`iterate - ${i+1}:`)
         if (posts.length > 0) {
+            const firstPost = posts[0];
             const lastPost = posts[posts.length - 1];
-            console.log(posts[0]);
+            lastId = lastPost.id;
+            if (isShowPost) {
+                console.log('first post:');
+                console.log(firstPost);
+            }
             console.log('last post id:', lastPost.id);
         }
         console.log(posts.length);
         i++;
     }
     console.log(JSON.stringify({ limit, total: postDataTotal, pageTotal: postPageTotal }));
-    console.timeEnd('command-find');
+    console.timeEnd(keyword);
 }
 
 main()
